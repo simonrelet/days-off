e1aead2679a0fab9b9af55be9c3824bfeea9ea99
Simon Relet <simon.relet+github@gmail.com>
2017-07-02 22:16:20 +0200

    apps: disable service workers

--- front-end/days-off/src/index.js
+++ front-end/days-off/src/index.js
@@ -1,8 +1,7 @@
+// @flow
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
