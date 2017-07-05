4992bc073c34ae5890b1496597c4fbcb3a181707
Simon Relet <simon.relet+github@gmail.com>
2017-07-06 01:01:25 +0200

    remove lerna

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,5 +2,7 @@
 set -euo pipefail
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-cd "$SCRIPT_DIR/packages/back-end/"
+ROOT_DIR="$(dirname "$SCRIPT_DIR")"
+
+cd "$ROOT_DIR/packages/back-end/"
 node build
