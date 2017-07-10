783da3950a170365bce2916018150d957ea11608
Simon Relet <simon.relet+github@gmail.com>
2017-07-10 22:35:06 +0200

    using jss

--- packages/components/lib/MarvApp/index.js
+++ packages/components/lib/MarvApp/index.js
@@ -1,12 +1,38 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import injectSheet from 'react-jss';
 
-export default function MarvApp({ header, content, app }) {
+const styles = {
+  app: {
+    display: 'flex',
+    flex: { direction: 'column' },
+    height: '100%',
+  },
+
+  header: {
+    flex: 'none',
+  },
+
+  content: {
+    flex: 1,
+    overflow: 'auto',
+  },
+
+  footer: {
+    color: '#aaa',
+    flex: 'none',
+    fontSize: '1.2rem',
+    padding: ['1rem', '1.6rem'],
+    textAlign: 'center',
+  },
+};
+
+function MarvApp({ header, content, app, classes }) {
   return (
-    <div className="MarvApp">
-      {header && React.createElement(header, { className: 'MarvApp__header' })}
-      {React.createElement(content, { className: 'MarvApp__content' })}
-      <div className="MarvApp__footer">
+    <div className={classes.app}>
+      {header && React.createElement(header, { className: classes.header })}
+      {React.createElement(content, { className: classes.content })}
+      <div className={classes.footer}>
         {app.name}@{app.version}
       </div>
     </div>
@@ -21,3 +47,5 @@ MarvApp.propTypes = {
   content: PropTypes.func.isRequired,
   header: PropTypes.func,
 };
+
+export default injectSheet(styles)(MarvApp);
--- packages/components/lib/Paper/index.js
+++ packages/components/lib/Paper/index.js
@@ -1,11 +1,70 @@
 import React from 'react';
 import classnames from 'classnames';
+import injectSheet from 'react-jss';
+import color from 'color';
 
-export default function Paper({ children, className, zDepth = 1 }) {
-  const classNames = classnames('Paper', `Paper--${zDepth}`, className);
+function alpha(a) {
+  return color().alpha(a).string();
+}
+
+const styles = {
+  paper: {
+    background: { color: '#fff' },
+    border: { radius: '.2rem' },
+    transition: {
+      property: 'all',
+      duration: 450,
+      timingFunction: 'cubic-bezier(0.23, 1, 0.32, 1)',
+    },
+  },
+
+  depth1: {
+    boxShadow: [
+      { y: 1, blur: 6, color: alpha(0.12) },
+      { y: 1, blur: 4, color: alpha(0.12) },
+    ],
+  },
+
+  depth2: {
+    boxShadow: [
+      { y: 3, blur: 10, color: alpha(0.16) },
+      { y: 3, blur: 10, color: alpha(0.23) },
+    ],
+  },
+
+  depth3: {
+    boxShadow: [
+      { y: 10, blur: 30, color: alpha(0.19) },
+      { y: 6, blur: 10, color: alpha(0.23) },
+    ],
+  },
+
+  depth4: {
+    boxShadow: [
+      { y: 14, blur: 45, color: alpha(0.25) },
+      { y: 10, blur: 18, color: alpha(0.22) },
+    ],
+  },
+
+  depth5: {
+    boxShadow: [
+      { y: 19, blur: 60, color: alpha(0.3) },
+      { y: 15, blur: 20, color: alpha(0.22) },
+    ],
+  },
+};
+
+function Paper({ children, className, classes, zDepth = 1 }) {
+  const classNames = classnames(
+    classes.paper,
+    classes[`depth${zDepth}`],
+    className,
+  );
   return (
     <div className={classNames}>
       {children}
     </div>
   );
 }
+
+export default injectSheet(styles)(Paper);
